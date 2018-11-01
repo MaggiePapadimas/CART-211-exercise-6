5ebdd16816011f801ffb131e42ba8412ea215c04
maggie <maggie.papadimas@hotmail.com>
2018-11-01 13:22:45 -0400

    script.js line 24: changed vx & vx to 5 instead of 50

--- js/script.js
+++ js/script.js
@@ -21,7 +21,7 @@ function setup() {
   createCanvas(640,480); ////fixed spelling error: create
   noStroke();
   // Create a ball
-  ball = new Ball(width/2,height/2,50,50,10,50);
+  ball = new Ball(width/2,height/2,5,5,10,50); ///Fixed ball moved to fast = changed 50 to 5 for vx & vy
   // Create the right paddle with UP and DOWN as controls
   rightPaddle = new Paddle(width-10,height/2,10,600,10, DOWN_ARROW, UP_ARROW); ////Fixed swapped UP_ARROW with DOWN_ARROW
   // Create the left paddle with W and S as controls
