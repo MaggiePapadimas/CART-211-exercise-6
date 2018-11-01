5c3c7695724ba6ed34f9147ef32ebdaeac97add5
maggie <maggie.papadimas@hotmail.com>
2018-11-01 12:24:05 -0400

    paddle.js line 33: added ==

--- js/Ball.js
+++ js/Ball.js
@@ -30,7 +30,7 @@ Ball.prototype.update = function () { ///// Fixed spelling error: function ////f
   this.y = constrain(this.y,0,height-this.size);
 
   // Check for touching upper or lower edge and reverse velocity if so
-  if (this.y = 0 || this.y + this.size === height) {
+  if (this.y === 0 || this.y + this.size === height) { ////fixed added ==
     this.vy = -this.vy;
   }
 }
